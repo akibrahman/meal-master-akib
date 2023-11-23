d34b2d70c8a33ae6a137b574b9f3ea8aca4fe5f6
akibrahman <akibrahman5200@gmail.com>
2023-11-23 20:50:20 +0600

    Navbar created with requirements

--- src/Components/Shared/NavBar.jsx
+++ src/Components/Shared/NavBar.jsx
@@ -1,22 +1,53 @@
+import { useState } from "react";
 import Container from "./Container";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Container>
-      <nav className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <img className="w-10" src="/logo.png" alt="" />
-          <p>MealMaster</p>
-        </div>
-        <div className="flex items-center gap-3">
-          <p>Home</p>
-          <p>Meals</p>
-          <p>Upcoming Meals</p>
-          <p>Join Us</p>
-          <img className="w-10" src="/no-user.jpg" alt="" />
-        </div>
-      </nav>
-    </Container>
+    <div className="bg-gradient-to-r from-primary to-secondary">
+      <Container>
+        <nav className="flex items-center justify-between py-2">
+          <div className="flex items-center gap-3">
+            <img
+              className="w-10 h-10 rounded-full bg-white"
+              src="/logo.jpg"
+              alt=""
+            />
+            <p className="text-white font-semibold text-lg">MealMaster</p>
+          </div>
+          <div className="flex items-center gap-5 text-white font-medium">
+            <p>Home</p>
+            <p>Meals</p>
+            <p>Upcoming Meals</p>
+            <button className="bg-white font-bold text-secondary px-3 py-1 rounded-full">
+              Join Us
+            </button>
+            <div className="relative">
+              <img
+                onClick={() => setIsOpen(!isOpen)}
+                className="w-10 h-10 rounded-full cursor-pointer"
+                src="/no-user.jpg"
+                alt=""
+              />
+              <div
+                className={`bg-white border border-secondary text-primary p-4 rounded-md absolute translate-y-12 ${
+                  isOpen
+                    ? "top-0 opacity-100"
+                    : "top-[600%] opacity-0 pointer-events-none"
+                } right-1/2 translate-x-1/2 text-center ease-in-out transition-all duration-300`}
+              >
+                <p className="pb-2 w-max select-none">Akib Rahman</p>
+                <hr />
+                <p className="pt-2 cursor-pointer">Dashboard</p>
+                <button className="bg-secondary text-white px-3 rounded-full mt-4">
+                  Logout
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </Container>
+    </div>
   );
 };
 
