dbb7c2d3c61d4df90611acd9783d07779f9f6cc3
akibrahman <akibrahman5200@gmail.com>
2023-11-27 16:14:19 +0600

    User can update his/her about me section from his profile, this functionality completed

--- src/Layouts/User/MyProfile.jsx
+++ src/Layouts/User/MyProfile.jsx
@@ -1,5 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { MdBloodtype } from "react-icons/md";
+import Modal from "react-modal";
+import { toast } from "react-toastify";
 import Loader from "../../Components/Shared/Loader";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import { AuthContext } from "../../Providers/AuthProvider";
@@ -8,8 +11,14 @@ import { convertCamelCaseToCapitalized } from "../../Utils/camelToCapitalize";
 const MyProfile = () => {
   const { user: authUser } = useContext(AuthContext);
   const axiosInstance = useAxiosPublic();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [error, setError] = useState(false);
 
-  const { data: user, isLoading } = useQuery({
+  const {
+    data: user,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["my-profile"],
     queryFn: async () => {
       const res = await axiosInstance.get(
@@ -20,10 +29,178 @@ const MyProfile = () => {
     enabled: authUser ? true : false,
   });
 
+  function isValidBloodGroup(input) {
+    const bloodGroupPattern = /^(A|B|AB|O)[+-]$/;
+    return bloodGroupPattern.test(input);
+  }
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#151515",
+      color: "#fff",
+      borderRadius: "20px",
+    },
+  };
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  const handleProfileUpdate = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    if (!isValidBloodGroup(form.bloodGroup.value)) {
+      setError("Enter Valid Blood Group Like 'A+','O-','AB-'");
+      return;
+    }
+    const data = {
+      area: form.area.value,
+      city: form.city.value,
+      division: form.division.value,
+      zipCode: form.zipCode.value,
+      phoneNo: form.phoneNo.value,
+      facebook: form.facebook.value,
+      bloodGroup: form.bloodGroup.value,
+      myself: form.myself.value,
+    };
+    const res = await axiosInstance.patch(
+      `/update-my-profile/${user.email}`,
+      data
+    );
+    if (res.data.acknowledged) {
+      toast.success("Profile Updated");
+      setModalIsOpen(false);
+      refetch();
+    } else {
+      toast.error("Something went Wrong");
+    }
+  };
+
   if (!user || isLoading || !authUser) return <Loader />;
   return (
-    <div className="">
-      <p className="font-semibold text-xl text-white bg-primary py-2 text-center w-[900px]">
+    <div className="w-[900px] my-20">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <form onSubmit={handleProfileUpdate}>
+          <div className="flex gap-6">
+            <div className="space-y-3">
+              <div className="flex items-center">
+                <label className="w-[100px] block">Area:</label>
+                <input
+                  defaultValue={user.about.area}
+                  placeholder="Name of your Area"
+                  className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+                  type="text"
+                  name="area"
+                />
+              </div>
+              <div className="flex items-center">
+                <label className="w-[100px] block">City:</label>
+                <input
+                  defaultValue={user.about.city}
+                  placeholder="Name of your City"
+                  className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+                  type="text"
+                  name="city"
+                />
+              </div>
+              <div className="flex items-center">
+                <label className="w-[100px] block">Division:</label>
+                <input
+                  defaultValue={user.about.division}
+                  placeholder="Name of your Division"
+                  className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+                  type="text"
+                  name="division"
+                />
+              </div>
+              <div className="flex items-center">
+                <label className="w-[100px] block">Zip Code:</label>
+                <input
+                  defaultValue={user.about.zipCode}
+                  placeholder="Enter your Postal Code"
+                  className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+                  type="number"
+                  name="zipCode"
+                />
+              </div>
+            </div>
+            <div className="space-y-3">
+              <div className="flex items-center">
+                <label className="w-[100px] block">Phone No :</label>
+                <input
+                  defaultValue={user.about.phoneNo}
+                  placeholder="Enter your Phone Number"
+                  className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+                  type="number"
+                  name="phoneNo"
+                />
+              </div>
+              <div className="flex items-center">
+                <label className="w-[100px] block">FB Link:</label>
+                <input
+                  defaultValue={user.about.facebook}
+                  placeholder="Enter your Facebook Link"
+                  className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+                  type="text"
+                  name="facebook"
+                />
+              </div>
+              <div className="flex items-center">
+                <label className="w-[100px] block">Blood Group:</label>
+                <input
+                  onChange={(e) => {
+                    if (isValidBloodGroup(e.target.value)) {
+                      setError("");
+                    } else {
+                      setError("Enter Valid Blood Group Like 'A+','O-','AB-'");
+                    }
+                  }}
+                  defaultValue={user.about.bloodGroup}
+                  placeholder="Your Blood Group"
+                  className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+                  type="text"
+                  name="bloodGroup"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 mt-3">
+            <label className="w-[100px] block">Myself:</label>
+            <textarea
+              defaultValue={user.about.myself}
+              placeholder="Write something about yourself"
+              className="rounded-md px-3 py-2 text-black font-semibold focus:outline-none"
+              rows="5"
+              name="myself"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="bg-green-500 text-white font-semibold px-3 py-2 transition-all active:scale-90 rounded-md cursor-pointer select-none mt-5"
+          >
+            Update
+          </button>
+          <button
+            onClick={() => setModalIsOpen(false)}
+            className="bg-red-500 text-white font-semibold px-3 py-2 transition-all active:scale-90 rounded-md cursor-pointer select-none mt-5 ml-5"
+          >
+            Cancle
+          </button>
+          {error && (
+            <p className="text-red-600 font-semibold py-[2px]">{error}</p>
+          )}
+        </form>
+      </Modal>
+      <p className="font-semibold text-xl text-white bg-primary py-2 text-center w-full">
         My profile
       </p>
       <div className="relative flex items-center justify-center gap-10 my-16">
@@ -86,6 +263,90 @@ const MyProfile = () => {
       <p className="font-semibold text-xl text-white bg-primary py-2 text-center w-[900px]">
         About Me
       </p>
+
+      <div className="mt-6 w-full">
+        <div className="flex items-start gap-6 w-full">
+          <div className="w-1/2">
+            <p className="font-medium py-1 text-white bg-primary text-center mb-2">
+              Address
+            </p>
+            <div className="flex items-center gap-2">
+              <label className="font-bold" htmlFor="">
+                Area:
+              </label>
+              <p>{user.about.area ? user.about.area : "Not Entered"}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <label className="font-bold" htmlFor="">
+                City:
+              </label>
+              <p>{user.about.city ? user.about.city : "Not Entered"}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <label className="font-bold" htmlFor="">
+                Division:
+              </label>
+              <p>{user.about.division ? user.about.division : "Not Entered"}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <label className="font-bold" htmlFor="">
+                Zip Code:
+              </label>
+              <p>{user.about.zipCode ? user.about.zipCode : "Not Entered"}</p>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <p className="font-medium py-1 text-white bg-primary text-center mb-2">
+              Contacts
+            </p>
+            <div className="flex items-center gap-2">
+              <label className="font-bold" htmlFor="">
+                Phone No.:
+              </label>
+              <p>{user.about.phoneNo ? user.about.phoneNo : "Not Entered"}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <label className="font-bold" htmlFor="">
+                Email:
+              </label>
+              <p>{user.email}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <label className="font-bold" htmlFor="">
+                Facebook:
+              </label>
+              <p>{user.about.facebook ? user.about.facebook : "Not Entered"}</p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex gap-6 w-full">
+          <div className="w-1/2">
+            <label className="font-medium py-1 text-white bg-primary text-center px-10">
+              Blood Groop
+            </label>
+            <p className="mt-3 flex items-center gap-2 text-2xl font-semibold">
+              <MdBloodtype className="text-red-600" />{" "}
+              {user.about.bloodGroup ? user.about.bloodGroup : "Not Entered"}
+            </p>
+          </div>
+          <div className="w-1/2">
+            <label className="font-medium py-1 text-white bg-primary text-center px-10">
+              About Myself
+            </label>
+            <p className="mt-3">
+              {user.about.myself ? user.about.myself : "Not Entered"}
+            </p>
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          setModalIsOpen(true);
+        }}
+        className="bg-red-500 text-white font-semibold px-3 py-2 transition-all active:scale-90 rounded-md cursor-pointer select-none mt-10"
+      >
+        Edit
+      </button>
     </div>
   );
 };
