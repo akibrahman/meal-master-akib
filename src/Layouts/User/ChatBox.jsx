b5d9f5c0716de0e0d1c349b49daa1b688dfb6f89
akibrahman <akibrahman5200@gmail.com>
2023-12-27 16:22:18 +0600

    Admin chatting ui completed

--- src/Layouts/User/ChatBox.jsx
+++ src/Layouts/User/ChatBox.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
-const ChatBox = () => {
+const ChatBox = ({ activeConversation }) => {
   const scroll = useRef();
 
   const [messages, setMessages] = useState([
@@ -54,53 +54,76 @@ const ChatBox = () => {
     },
   ]);
 
-  useEffect(() => {
-    scroll.current.scrollTop = scroll.current.scrollHeight;
-  }, [messages]);
+  // useEffect(() => {
+  //   scroll.current.scrollTop = scroll.current.scrollHeight;
+  // }, [messages]);
 
   const handleSendMessage = (message) => {
     setMessages([...messages, message]);
   };
 
   return (
-    <div
-      ref={scroll}
-      className="overflow-y-scroll border rounded h-full bg-white relative p-4 pb-0"
-    >
-      {/* Messages */}
-      <div className="mb-4 flex flex-col">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={`mb-2 bg-black text-white px-3 py-1 font-medium w-max max-w-xs text-center rounded-md ${
-              message.sender === "Person 1" ? "self-start" : "self-end"
-            }`}
-          >
-            {message.text}
-          </div>
-        ))}
-      </div>
-      {/* Message input */}
-      <div className="flex sticky w-full bottom-0 left-0 bg-white p-2 pt-">
-        <input
-          type="text"
-          className="flex-1 border p-2 rounded"
-          placeholder="Type your message..."
-        />
-        <button
-          className="bg-[#FFBE00] text-white p-2 ml-2 rounded"
-          onClick={() =>
-            handleSendMessage({
-              id: messages.length + 1,
-              text: "New message",
-              sender: "Person 1", // Change sender as needed
-            })
-          }
+    <>
+      {activeConversation ? (
+        <div
+          ref={scroll}
+          className="overflow-y-scroll rounded h-full bg-white relative"
         >
-          Send
-        </button>
-      </div>
-    </div>
+          <div className="bg-black sticky top-0 flex items-center gap-5 rounded-sm py-2 px-5 text-white">
+            {/* <div className=""> */}
+            <img
+              className="w-10 h-10 rounded-full"
+              src={activeConversation?.photo}
+              alt=""
+            />
+            <div className="">
+              <p className="text-lg">{activeConversation?.name}</p>
+              <p className="text-xs">{activeConversation?.email}</p>
+            </div>
+            {/* </div> */}
+          </div>
+          {/* Messages */}
+          <div className="mb-4 p-4 flex flex-col">
+            {messages.map((message) => (
+              <div
+                key={message.id}
+                className={`mb-2 bg-black text-white px-3 py-1 font-medium w-max max-w-xs text-center rounded-md ${
+                  message.sender === "Person 1" ? "self-start" : "self-end"
+                }`}
+              >
+                {message.text}
+              </div>
+            ))}
+          </div>
+          {/* Message input */}
+          <div className="flex sticky w-full bottom-0 left-0 bg-black rounded-sm p-2">
+            <input
+              type="text"
+              className="flex-1 border p-2 rounded-sm"
+              placeholder="Type your message..."
+            />
+            <button
+              className="bg-[#FFBE00] text-white p-2 ml-2 rounded-sm"
+              onClick={() =>
+                handleSendMessage({
+                  id: messages.length + 1,
+                  text: "New message",
+                  sender: "Person 1", // Change sender as needed
+                })
+              }
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full h-full flex items-center justify-center border border-black">
+          <p className="text-xl font-medium select-none">
+            Click one conversation to start chat
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/Layouts/User/UserChat.jsx
+++ src/Layouts/User/UserChat.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import ChatBox from "./ChatBox";
 
 const UserChat = () => {
+  const [activeConversation, setActiveConversation] = useState(null);
+  const users = [
+    {
+      name: "Akib Rahman",
+      email: "akibrahman5200@gmail.com",
+      photo: "https://i.ibb.co/8YfgbmZ/Linkdin1.jpg",
+    },
+    {
+      name: "Suchona Islam",
+      email: "suchona.islam.shila@gmail.com",
+      photo: "https://i.ibb.co/swPt0Sv/IMG-20230102-WA0016.jpg",
+    },
+    {
+      name: "Jhon Denim",
+      email: "jhon.denim.web@gmail.com",
+      photo: "https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp",
+    },
+  ];
   return (
     <div className="flex gap-4 w-[98%] h-[90vh] conversations-scrollbar">
       <div className="w-1/4 bg-[#141515] text-white p-2 rounded-sm overflow-y-scroll scrollbar-hide">
@@ -10,146 +29,28 @@ const UserChat = () => {
         </div>
         <div className="flex flex-col gap-5">
           {/* Conversations  */}
-          <div className="flex items-center gap-5 bg-stone-500 rounded-sm p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/8YfgbmZ/Linkdin1.jpg"
-              alt=""
-            />
-            <div className="">
-              <p>Akib Rahman</p>
-              <p className="text-[9px]">akibrahman5200@gmail.com</p>
+          {users.map((user, i) => (
+            <div
+              onClick={() => setActiveConversation(user)}
+              key={i}
+              className={`flex items-center gap-5 rounded-sm p-2 cursor-pointer ${
+                user.email === activeConversation?.email
+                  ? "bg-stone-600"
+                  : "hover:bg-stone-700"
+              }`}
+            >
+              <img className="w-10 h-10 rounded-full" src={user.photo} alt="" />
+              <div className="">
+                <p>{user.name}</p>
+                <p className="text-[9px]">{user.email}</p>
+              </div>
             </div>
-          </div>
-
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="!w-10 h-10 rounded-full"
-              src="https://i.ibb.co/swPt0Sv/IMG-20230102-WA0016.jpg"
-              alt=""
-            />
-            <div className="">
-              <p>Suchona Islam</p>
-              <p className="text-[9px]">suchona.islam.shila@gmail.com</p>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          {/* sdhljsjdlsdlsdlsdklsdjkl  */}
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 p-2">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://i.ibb.co/yBhSVPW/istockphoto-1399565382-170667a.webp"
-              alt=""
-            />
-            <div className="">
-              <p>Jhon Denim</p>
-              <p className="text-[9px]">jhon.denim.web@gmail.com</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className="flex-1 relative">
         {/* Chat box */}
-        <ChatBox />
+        <ChatBox activeConversation={activeConversation} />
       </div>
     </div>
   );
